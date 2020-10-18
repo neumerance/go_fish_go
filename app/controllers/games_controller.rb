9314c0596c1acd32f2f847058631083952dd33e1
Jonathan Canaveral <jonathan.canaveral@moneysmart.com>
2020-10-18 21:16:23 +0800

    add endpoint for fishing

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,7 +3,6 @@ class GamesController < ApplicationController
 
   api :POST, '/games'
   param :player_ids, Array, desc: 'Array of players IDs that will join the new game'
-
   def create
     interactor = Game::Create.run({ players: @players })
     render json: interactor.game
@@ -11,20 +10,30 @@ class GamesController < ApplicationController
 
   api :POST, '/games/:id'
   param :id, Integer, desc: 'Game ID'
-
   def show
     render json: @game
   end
 
   api :GET, '/games/:id/start'
   param :id, Integer, desc: 'Game ID'
-
   def start
     interactor = Game::Start.run({ game: @game })
     render status: :ok
   end
 
+  api :GET, '/games/:id/fish'
+  param :id, Integer, desc: 'Game ID'
+  param :game_player_id, Integer, desc: 'Game Player ID requesting for a card'
+  param :target_game_player_id, Integer, desc: 'Target Game Player ID requested for a card'
+  param :card_value, Integer, desc: 'Request card value'
   def fish
+    interactor = Game::Fish.run({
+      game: @game,
+      current_game_player: GamePlayer.find(params[:game_player_id]),
+      game_player: GamePlayer.find(params[:target_game_player_id]),
+      card_value: params[:card_value]
+    })
+    render status: :ok
   end
 
   private
